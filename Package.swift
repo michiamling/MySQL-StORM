e5637ad9050f9a4191169dc0b56530d0b474fa23
Michael Amling <michael.amling@amcom.at>
2020-04-19 12:18:58 +0200

    update package

--- Package.swift
+++ Package.swift
@@ -6,10 +6,13 @@
 import PackageDescription
 let package = Package(
     name: "MySQLStORM",
+    products: [.library(name: "MySQLStORM", targets: ["MySQLStORM"])],
     dependencies: [
         .package(url: "https://github.com/michiamling/Perfect-MySQL.git", from: "2.0.0"),
         .package(url: "https://github.com/SwiftORM/StORM.git", from: "3.0.0"),
         .package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", from: "3.0.0"),
     ],
-    targets: []
+    targets: [
+        .target(name: "MySQLStORM", dependencies: ["Perfect-MySQL", "StORM", "Perfect-Logger"])
+    ]
 )
