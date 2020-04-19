543bb7f90f2a2381ad61a62451cfd3d48b097f6b
Michael Amling <michael.amling@amcom.at>
2020-04-19 13:00:49 +0200

    update source and packages

--- Package.swift
+++ Package.swift
@@ -3,16 +3,17 @@
 // Generated automatically by Perfect Assistant Application
 // Date: 2017-01-12 22:15:25 +0000
 
+
 import PackageDescription
 let package = Package(
     name: "MySQLStORM",
     products: [.library(name: "MySQLStORM", targets: ["MySQLStORM"])],
     dependencies: [
-        .package(url: "https://github.com/michiamling/Perfect-MySQL.git", from: "2.0.0"),
-        .package(url: "https://github.com/SwiftORM/StORM.git", from: "3.0.0"),
-        .package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", from: "3.0.0"),
+        .package(name: "PerfectMySQL", url: "https://github.com/michiamling/Perfect-MySQL.git", from: "2.0.0"),
+        .package( url: "https://github.com/SwiftORM/StORM.git", from: "3.0.0"),
+        .package(name: "PerfectLogger", url: "https://github.com/PerfectlySoft/Perfect-Logger.git", from: "3.0.0"),
     ],
     targets: [
-        .target(name: "MySQLStORM", dependencies: ["Perfect-MySQL", "StORM", "Perfect-Logger"])
+        .target(name: "MySQLStORM", dependencies: ["PerfectMySQL", "StORM", "PerfectLogger"])
     ]
 )
